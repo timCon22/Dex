5464ac883d14a0a20c1b0fc74d270b516645a058
timCon22 <timconat1@gmail.com>
2022-11-10 18:48:48 -0700

    add heroku psql url

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///flask-heroku'))
+    os.environ.get('DATABASE_URL', 'postgresql:///pokemon'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
